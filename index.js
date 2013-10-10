3b06ce0f8badcd0653aadd6a141be76ad12e8472
Ben McMahen <ben.mcmahen@gmail.com>
2013-10-09 20:57:16 -0600

    remove weird garbled text... wat??

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /**
  * Module dependencies.
  */
@@ -20,19 +21,11 @@ module.exports = Overlay;
  * @api public
  */
 
-<<<<<<< HEAD
 function Overlay(className) {
   if (!(this instanceof Overlay)) return new Overlay();
   this.el = document.createElement('div');
   if (className) classes(this.el).add(className);
   this.el.id = 'overlay';
-=======
-function Overlay(options) {
-  if (!(this instanceof Overlay)) return new Overlay(options);
-  options || (options = {});
-  this.duration = options.duration || 300;
-  this.id = options.id || 'overlay';
->>>>>>> 8a4b438dbe3352c6c52b42f06d8b379b5b6753d3
 }
 
 /**
@@ -52,12 +45,6 @@ Emitter(Overlay.prototype);
 
 Overlay.prototype.show = function(){
   if (this.el) return;
-<<<<<<< HEAD
-=======
-  this.el = document.createElement('div');
-  this.el.className = 'hide';
-  this.el.id = this.id;
->>>>>>> 8a4b438dbe3352c6c52b42f06d8b379b5b6753d3
   document.getElementsByTagName('body')[0].appendChild(this.el);
   this.emit('show');
   redraw(this.el);
